d8980e697bf37153e3947490d674d1098b575e21
shigure <chen970129349@qq.com>
2020-07-16 16:17:46 +0800

    test

---
+++ src/main/java/com/example/demo2/entity/Dog.java
@@ -0,0 +1,31 @@
+package com.example.demo2.entity;
+
+
+public class Dog {
+    private int age;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
